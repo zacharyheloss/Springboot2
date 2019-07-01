41df88b4539e804ddbb2b30a2361fb4988bffe62
zhangcheng <chengzhangd@yeah.net>
2019-07-01 11:44:58 +0800

    删除无效的

--- src/main/java/com/zachary/springboot/blog/pushlian/dao/EsblogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zachary.springboot.blog.pushlian.dao;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import com.zachary.springboot.blog.pushlian.damain.Esblog;
-
-public interface EsblogRepository extends ElasticsearchRepository<Esblog, String>{
-	
-}
